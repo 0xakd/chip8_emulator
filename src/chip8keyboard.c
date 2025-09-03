068bfde339c37582f9c6cc7fbe5acb799427ab26
0xAKD <helloajaydey@gmail.com>
2025-09-03 18:17:50 +0530

    completed implementing chip8 keyboard

--- src/chip8keyboard.c
+++ src/chip8keyboard.c
@@ -1,4 +1,11 @@
 #include "chip8keyboard.h"
+#include <assert.h>
+
+
+static void chip8_keyboard_ensure_in_bounds(int key){
+    assert(key > 0 && key < CHIP8_TOTAL_KEYS);
+}
+
 
 int chip8_keyboard_map(const char* map, char key){
     for(int i = 0; i < CHIP8_TOTAL_KEYS; i++){
--- src/main.c
+++ src/main.c
@@ -2,34 +2,70 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
 #include "chip8.h"
+#include "chip8keyboard.h"
+
+const char keyboard_map[CHIP8_TOTAL_KEYS] = {
+    SDLK_0, SDLK_1, SDLK_2, SDLK_3, SDLK_4, SDLK_5,
+    SDLK_6, SDLK_7, SDLK_8, SDLK_9, SDLK_a, SDLK_b,
+    SDLK_c, SDLK_d, SDLK_e, SDLK_f
+};
+
 
 int main(int argc, char const** argv){
 
     struct chip8 chip8;
-    chip8.resistors.SP = 0;
+    chip8_init(&chip8);
 
-    chip8_stack_push(&chip8, 0xbb);
-    chip8_stack_push(&chip8, 0xaa);
+    chip8_keyboard_down(&chip8.keyboard, 0x0f);
+    chip8_keyboard_up(&chip8.keyboard, 0x0f);
 
-    printf("%x\n", chip8_stack_pop(&chip8));
-    printf("%x\n", chip8_stack_pop(&chip8));
 
+    bool is_down = chip8_keyboard_is_down(&chip8.keyboard, 0x0f);
+    printf("%i\n", (int)is_down);
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
         "Chip8 Window",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        CHIP8_WIDTH * CHIP8_WINDOWS_MULTIPLIER,  CHIP8_HEIGHT * CHIP8_WINDOWS_MULTIPLIER, SDL_WINDOW_SHOWN
+        CHIP8_WIDTH * CHIP8_WINDOWS_MULTIPLIER,
+        CHIP8_HEIGHT * CHIP8_WINDOWS_MULTIPLIER, SDL_WINDOW_SHOWN
     );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
     while (1){
         SDL_Event event;
         while (SDL_PollEvent(&event)){
-            if(event.type == SDL_QUIT){
+
+            switch (event.type)
+            {
+            case SDL_QUIT:
                 goto out;
+                break;
+            
+            case SDL_KEYDOWN:
+                {
+                    char key = event.key.keysym.sym;
+                    int vkey = chip8_keyboard_map(keyboard_map, key);
+                    if(vkey != -1){
+                        chip8_keyboard_down(&chip8.keyboard, vkey);
+                    }
+                }
+                break;
+
+            case SDL_KEYUP:
+                {
+                    char key = event.key.keysym.sym;
+                    int vkey = chip8_keyboard_map(keyboard_map, key);
+                    if(vkey != -1){
+                        chip8_keyboard_down(&chip8.keyboard, vkey);
+                    }
+                }
+                break;
+            default:
+                break;
             }
+
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
